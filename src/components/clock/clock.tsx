7a046631e9d989d0587b1325416f00fd3aee36cc
0xNordian <85548493+0xNordian@users.noreply.github.com>
2023-07-26 13:24:14 +0200

    clock css

--- src/components/clock/clock.tsx
+++ src/components/clock/clock.tsx
@@ -1,27 +1,28 @@
 import { useState, useEffect } from "react";
+import styles from './clock.module.css'
 
 function Clock() {
-  const [time, setTime] = useState<number>(0);
+    const [time, setTime] = useState<number>(0);
 
-  useEffect(() => {
-    // Update the state with the current time every second
-    const intervalId = setInterval(() => {
-      const currentDate = new Date();
-      setTime(currentDate.getTime());
-    }, 1000);
+    useEffect(() => {
+        // Update the state with the current time every second
+        const intervalId = setInterval(() => {
+            const currentDate = new Date();
+            setTime(currentDate.getTime());
+        }, 1000);
 
-    // Clean up the interval when the component is unmounted
-    return () => clearInterval(intervalId);
-  }, []);
+        // Clean up the interval when the component is unmounted
+        return () => clearInterval(intervalId);
+    }, []);
 
-  // Convert the time (in milliseconds) to a human-readable format
-  const formattedTime = new Date(time).toLocaleTimeString();
+    // Convert the time (in milliseconds) to a human-readable format
+    const formattedTime = new Date(time).toLocaleTimeString();
 
-  return (
-    <div>
-      <p>Current time: {formattedTime}</p>
-    </div>
-  );
+    return (
+        <div>
+            <p className={styles.clockTest}>{formattedTime}</p>
+        </div>
+    );
 }
 
 export default Clock;
